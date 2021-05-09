a5819612754ad1128632f4f8fe1c9b2fc9115505
ShrinivasSK <shrinivaskhistesk@gmail.com>
2021-05-09 18:09:23 +0530

    Lyrics Scraper for different websites added

--- LyricsFinder/LyricsFinder/spiders/lyris_finder.py
+++ LyricsFinder/LyricsFinder/spiders/lyris_finder.py
@@ -5,17 +5,17 @@ from scrapy.utils.response import open_in_browser
 '''
 Websites I can  scrape:
 1. Genius
-2. Lyrics.com
+2. azlyrics - does not allow
 3. Metrolyrics
 
-4. Lyricsily
-5. Jiosaavn
+4. Lyricsily - gives garbage too 
+5. Jiosaavn - does not allow
 6. Gaana
 
-7. hinditracks
-8. Lyricsmint
-9. Jiosaavn
+7. Lyricsmint - done
+8. ilyricshub - done
 '''
+
 
 class LyricsFinderSpider(scrapy.Spider):
     name = 'lyris_finder'
@@ -29,18 +29,58 @@ class LyricsFinderSpider(scrapy.Spider):
 
     def parse(self, response):
         xlink = LinkExtractor()
-        link=[link for link in xlink.extract_links(response) if str(link.url).find('genius')!=-1]
-        if(len(link)!=0):
+        link = [link for link in xlink.extract_links(
+            response) if str(link.url).find('lyricsmint') != -1]
+        if(len(link) != 0):
             print(link[0].url)
-            yield scrapy.Request(link[0].url,callback=self.parseGenius)
-        # else:
-        # for link in xlink.extract_links(response):
-        # # print("Yo"+str(link),'lyrics' in link.url,'lyrics' in link.text)
-        #     if 'lyrics' in link.url and 'lyrics' in str(link.text).lower():
-        #         yield {'link': str(link)}
-    
-    def parseGenius(self,response):
-        # open_in_browser(response)
-        lyrics=response.css('.lyrics p').css('::text').extract()
-        yield {'lyrics': lyrics}
+            yield scrapy.Request(link[0].url, callback=self.parseLyricsMint)
+        link = [link for link in xlink.extract_links(
+            response) if str(link.url).find('gaana') != -1]
+        if(len(link) != 0):
+            print(link[0].url)
+            yield scrapy.Request(link[0].url, callback=self.parseGaana)
+        link = [link for link in xlink.extract_links(
+            response) if str(link.url).find('metrolyrics') != -1]
+        if(len(link) != 0):
+            print(link[0].url)
+            yield scrapy.Request(link[0].url, callback=self.parseMetrolyics)
+        link = [link for link in xlink.extract_links(
+            response) if str(link.url).find('genius') != -1]
+        if(len(link) != 0):
+            print(link[0].url)
+            yield scrapy.Request(link[0].url, callback=self.parseGenius)
+        # link = [link for link in xlink.extract_links(
+        #     response) if str(link.url).find('lyricsily') != -1]
+        # if(len(link) != 0):
+        #     print(link[0].url)
+        #     yield scrapy.Request(link[0].url, callback=self.parseLyricsily)
+        link = [link for link in xlink.extract_links(
+            response) if str(link.url).find('ilyricshub') != -1]
+        if(len(link) != 0):
+            print(link[0].url)
+            yield scrapy.Request(link[0].url, callback=self.parseILyricsHub)
 
+    def parseGenius(self, response):
+        # open_in_browser(response)
+        lyrics = response.css('.lyrics p').css('::text').extract()
+        yield {'lyrics': lyrics, 'source': 'Genius'}
+
+    def parseLyricsily(self, response):
+        lyrics = response.css('p::text').extract()
+        yield {'lyrics': lyrics, 'source': 'Lyricsily'}
+
+    def parseGaana(self, response):
+        lyrics = response.css('.seelyrics p').css('::text').extract()
+        yield {'lyrics': lyrics, 'source': 'Gaana'}
+
+    def parseMetrolyics(self, response):
+        lyrics = response.css('.verse::text').extract()
+        yield {'lyrics': lyrics, 'source': 'Metrolyrics'}
+
+    def parseILyricsHub(self, response):
+        lyrics = response.css('.song_lyrics p').css('::text').extract()
+        yield {'lyrics': lyrics, 'source': 'I Lyrics Hub'}
+
+    def parseLyricsMint(self, response):
+        lyrics = response.css('.pb-2 p').css('::text').extract()
+        yield {'lyrics': lyrics, 'source': 'Lyrics Mint'}
