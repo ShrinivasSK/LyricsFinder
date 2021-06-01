014b526ddb8b2d1f26d31ebf29c39cc1304efe81
ShrinivasSK <shrinivaskhistesk@gmail.com>
2021-06-01 19:10:45 +0530

    Add Heroku deployment files

--- app.py
+++ app.py
@@ -21,27 +21,6 @@ def start():
     return render_template('index.html')
 
 
-@app.route('/login',methods=['GET','POST'])
-def login():
-    url='https://accounts.spotify.com/authorize'
-    params={
-        'client_id': '3e341320482944b19eeeb62bb5f7d11c',
-        'response_type': 'code',
-        'redirect_uri': 'http://127.0.0.1:5000/result',
-        'scope': 'user-read-currently-playing user-read-playback-state',
-    }
-    r=requests.get(url=url,params=params)
-    print(r.json())
-    return jsonify(result="done")
-
-
-@app.route('/result',methods=['GET','POST'])
-def result():
-    print(request.get_json())
-    return render_template('index.html')
-
-
-
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
